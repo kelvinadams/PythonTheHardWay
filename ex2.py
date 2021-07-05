cf5a9f27edf06c39dd3514e2ce6994aa7aa91cb4
Kelvin Adams <83097984+kelvinadams@users.noreply.github.com>
2021-07-05 14:41:12 -0700

    Adding Exercises 1-3 via upload
    Learn Python the Hard Way Exercises 1-3

---
+++ ex2.py
@@ -0,0 +1,11 @@
+# Python the Hard Way - Exercise 2
+
+# Here is a test using a comment.
+# Huzzah for ignored lines of code!
+
+print("I could have code like this.")  # and everything after is ignored
+
+# Handy dandy tool for "disabling" lines of code
+# print("This won't run.")
+
+print("This will run!")
---
+++ ex3.py
@@ -0,0 +1,29 @@
+# Python the Hard Way - Exercise 3
+
+print("I will now count my chickens:")
+
+# These print text compbined with mathematical expressions
+print("Hens", 25 + 30 / 6)
+print("Roosters", 100-25 * 3 % 4)
+
+print("Now I will count my eggs:")
+
+# This illustration shows how Python follows mathematical order of operations: PEMDAS
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+
+# Another order of operations situation
+print("Is it true that 3 + 2 < 5 - 7?")
+
+print(3 + 2 < 5 - 7)
+
+print("What is 3 + 2?", 3 + 2)
+print("What is 5 - 7?", 5 - 7)
+
+print("By Jove, that's why it's False!")
+
+print("How about some more...comparing 5 & -2...")
+
+# Illustrating other operators in Python
+print("Is it greater?", 5 > -2)
+print("Is it greater or equal?", 5 >= -2)
+print("Is it less or equal?",  5 <= -2)
